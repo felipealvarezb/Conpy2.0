91804c95db585fcaee37b7d0bda9ae9c447c9965
jjsanchezc <jjsanchezc@eafit.edu.co>
2022-04-19 07:17:43 -0500

    cambios en la base de datos y organicé el codigo

--- clientes/models.py
+++ clientes/models.py
@@ -6,9 +6,9 @@ class Cliente(models.Model):
     )
     id = models.AutoField(primary_key=True)
     nombre_cliente=models.TextField(max_length=35,verbose_name='nombre_cliente')
-    cedula_cliente=models.IntegerField (max_length=15,verbose_name='cedula_cliente')
+    cedula_cliente=models.IntegerField (verbose_name='cedula_cliente')
     correo_cliente=models.TextField(max_length=35,verbose_name='correo_cliente')
-    celular_cliente=models.IntegerField (max_length=15,verbose_name='celular_cliente')
+    celular_cliente=models.IntegerField (verbose_name='celular_cliente')
     
     def __str__(self):
         fila='Nombre: '+self.nombre_cliente+'- Cedula: '+self.cedula_cliente + '- Correo: '+self.cedula_cliente+'- Celular: '+self.celular_cliente
--- negocio/views.py
+++ negocio/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Dato, Notificacion
-from django.core.mail import send_mail
 from .forms import DatoForm, NotificacionForm
-from negocio.notificaciones import manejoNoti as mn
 
 @login_required(login_url="/login/")
 def home(request):
@@ -14,18 +12,12 @@ def notificaciones(request):
     msg=None
     datos=Notificacion.objects.all()
     formulario = NotificacionForm(request.POST or None)
-    print(request.POST.get('fecha_lim'))
     if formulario.is_valid():
         formulario.save()
-        descripcion=request.POST.get('descripcion')
-        fecha=request.POST.get('fecha_lim')
         try:
-            #mn.enviar_mail_nueva_noti(descripcion,fecha)
             msg="La notificación se ha creado correctamente"
         except:
             msg="hubo un error"
-       
-
     return render(request,'negocio/notificaciones.html',{'datos':datos,'formulario':formulario,'msg':msg})
 
 @login_required(login_url="/login/")
@@ -36,14 +28,12 @@ def perfil(request):
 def ingresos(request):
     datos = Dato.objects.all()
     formulario = DatoForm(request.POST or None) 
-    
     if formulario.is_valid():
         formulario.save()
     return render(request, "negocio/crud/ingresos.html", {'datos': datos, 'formulario': formulario})
 
 @login_required(login_url="/login/")
 def editar(request):
-    #send_mail("este es el subjet",'este es el mensjaeeeee','jjsanchezc@eafit.edu.co',['jjsanchez1@hotmail.com'])
     return render(request, "negocio/crud/editar.html")
 
 @login_required(login_url="/login/")
